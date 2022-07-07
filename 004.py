82c52f4198102d9bfb354c80f4bafe54f4be6707
Denis <chekmarevdenchik5@gmail.com>
2022-07-07 17:01:35 +0700

    fourth task

---
+++ 004.py
@@ -0,0 +1,12 @@
+part = int(input('--> '))
+
+if part == 1:
+    print('X > 0 and Y > 0')
+elif part == 2:
+    print('X < 0 and Y > 0')
+elif part == 3:
+    print('X < 0 and Y < 0')
+elif part == 4:
+    print('X > 0 and Y < 0')
+else: 
+    print('Wrong input')
